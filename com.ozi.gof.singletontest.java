60cc99319d0242b96adb5714ee647eae9d7858f2
a-zinchenko <zinchenko_ai@mail.ru>
2017-09-19 00:29:49 +0300

    Create com.ozi.gof.singletontest.java

---
+++ com.ozi.gof.singletontest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import com.ozi.gof.singleton;
+
+class Singletontest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+    Singleton s = s.getInstance();
+    s.getInstance();
+	}
+}
