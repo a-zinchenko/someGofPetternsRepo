5cd879984f806537b3d78b26aa41f5dd50d0677f
a-zinchenko <zinchenko_ai@mail.ru>
2017-09-19 00:32:04 +0300

    Update com.ozi.gof.singleton.java

--- com.ozi.gof.singleton.java
+++ com.ozi.gof.singleton.java
@@ -4,7 +4,11 @@ private Singlton instance = null;
 private Singltone(){}
 
 public Singlton getInstance (){
-  if (instance == null) {instance = new Singltone();
+  if (instance == null) {
+    instance = new Singltone();
+    System.out.println("Singleton instance has been created.");
+  } else {
+    System.out.println("Used existed instanse.");
   }
   return instance;
 }
